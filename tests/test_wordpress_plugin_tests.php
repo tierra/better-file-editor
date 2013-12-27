589edb077c4849b4313d617237aa448d4c36286c
Bryan Petty <bryan@ibaku.net>
2013-12-27 15:20:13 -0700

    Fix some tests.

--- tests/test_wordpress_plugin_tests.php
+++ tests/test_wordpress_plugin_tests.php
@@ -19,19 +19,19 @@ class WP_Test_WordPress_Plugin_Tests extends WP_UnitTestCase {
 	/**
 	 * If these tests are being run on Travis CI, verify that the version of
 	 * WordPress installed is the version that we requested.
+	 *
+	 * @requires PHP 5.3
 	 */
 	function test_wp_version() {
 
-		if ( !getenv( 'TRAVIS_PHP_VERSION' ) )
+		if ( !getenv( 'TRAVIS' ) )
 			$this->markTestSkipped( 'Test skipped since Travis CI was not detected.' );
 
-		//grab the requested version
 		$requested_version = getenv( 'WP_VERSION' );
 
-		//trunk is always "master" in github terms, but WordPress has a specific way of describing it
-		//grab the exact version number to verify that we're on trunk
+		// The "master" version requires special handling.
 		if ( $requested_version == 'master' ) {
-			$file = file_get_contents( 'https://raw.github.com/WordPress/WordPress/master/wp-includes/version.php' );
+			$file = file_get_contents( 'https://raw.github.com/tierra/wordpress/master/src/wp-includes/version.php' );
 			preg_match( '#\$wp_version = \'([^\']+)\';#', $file, $matches );
 			$requested_version = $matches[1];
 		}
